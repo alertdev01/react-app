347e5f302181f5e577e7ff8d86654ef57b0ffc67
alertdev01 <104558179+alertdev01@users.noreply.github.com>
2022-05-02 22:06:47 +0200

    Update Message.jsx

--- src/components/message/Message.jsx
+++ src/components/message/Message.jsx
@@ -5,7 +5,7 @@ export default function Message({message, own }) {
     return (
         <div className={own ? "message own" : "message"}>
             <div className="messageTop">
-                <img className='messageImg' src="https://cdn.pixabay.com/photo/2017/04/13/22/33/maine-coon-cat-2228866_960_720.jpg" />
+                <img className='messageImg' src="https://cdn.pixabay.com/photo/2017/04/13/22/33/maine-coon-cat-2228866_960_720.jpg" alt="description of image"/>
                 <p className='messageText'>
                     {message.text}
                 </p>
